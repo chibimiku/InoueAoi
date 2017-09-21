a44a6d6630163ded9ddfe57f85f0e3384ef30be1
chibimiku <dreamcosine@gmail.com>
2017-09-21 19:03:22 +0800

    move indic

--- pythoncore/topic_analy.py
+++ pythoncore/topic_analy.py
@@ -4,6 +4,8 @@
 import jieba
 import jieba.analyse
 
+import data_utils
+
 #把原始topic文件切分成关键词->topic
 def split_topic(in_file, out_file):
     line_count = 0
@@ -33,19 +35,28 @@ def split_2row(in_file, out_file1, out_file2):
         with open(out_file1, 'w+', encoding="utf8") as wfp1:
             with open(out_file2, 'w+', encoding="utf8") as wfp2:
                 for line in fp:
-                try:
-                    m_data = line.split("\t")
-                    to_data = m_data[1].split(",")
-                    if(not m_data):
-                        continue
-                    if(not to_data):
-                        continue
-                    wfp1.write(m_data[1] + "\n")
-                    wfp2.write(" ".join(to_data) + "\n")
-                    if(line_count > 0 and line_count % 10000 == 0):
-                        print ("run for " + str(line_count) + " line(s)")
-                except e:
-                    pass
+                    try:
+                        m_data = line.split("\t")
+                        to_data = m_data[1].split(",")
+                        if(not m_data):
+                            continue
+                        if(not to_data):
+                            continue
+                        wfp1.write(m_data[0].strip() + "\n")
+                        wfp2.write(" ".join(to_data).strip() + "\n")
+                        if(line_count > 0 and line_count % 10000 == 0):
+                            print ("run for " + str(line_count) + " line(s)")
+                    except:
+                        pass
 
+def do_word2vec():
+    my_len = 15000000
+    data_utils.create_vocabulary('data/topic/topic_index.vocal', 'data/topic/topic_index.txt', my_len)
+    data_utils.data_to_token_ids('data/topic/topic_index.txt', 'data/topic/topic_index.vec', 'data/topic/topic_index.vocal')
+    
+    data_utils.create_vocabulary('data/topic/topic_group.vocal', 'data/topic/topic_group.txt', my_len)
+    data_utils.data_to_token_ids('data/topic/topic_group.txt', 'data/topic/topic_group.vec', 'data/topic/topic_group.vocal')
+                        
 #split_topic('data/zhihu_topic_output', 'data/topic_output.txt')
-split_2row('data/topic_output.txt', 'data/topic_index.txt', 'data/topic_group.txt')
+#split_2row('data/topic/topic_output.txt', 'data/topic/topic_index.txt', 'data/topic/topic_group.txt')
+do_word2vec()
